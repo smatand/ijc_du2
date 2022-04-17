b3a20bb2e7890418d361dd7c3a82bf9c2eed4233
smatand <andrejsmatana0@gmail.com>
2022-04-17 11:12:27 +0200

    Edit htab_hash_function.c

--- htab_hash_function.c
+++ htab_hash_function.c
@@ -1,16 +1,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#include "htab.h"
-#include "htab_struct.h"
+#include "htab_private.h"
 
-/**
- * @brief Hash function for the hash table
- * 
- * @param str string to store in hash table
- * 
- * @return size_t index of item in hash table
- */
 size_t htab_hash_function(const char *str) {
     uint32_t h=0;     // integer has to be of 32 bits
     const unsigned char *p;
