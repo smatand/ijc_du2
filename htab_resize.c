88ed565db8ff04a7e94e09549fcc044ba3cf3316
smatand <andrejsmatana0@gmail.com>
2022-04-18 23:21:42 +0200

    Edit htab resize function.

--- htab_resize.c
+++ htab_resize.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <string.h>
 
 #include "htab_private.h"
@@ -8,6 +9,9 @@ void htab_resize(htab_t *t, size_t newn) {
 	}
 
 	htab_t *newt = htab_init(t->arr_size);
+	if (newt == NULL) {
+		goto newtAllocationError;
+	}
 
 	for (size_t i = 0; i < newt->arr_size; i++) {
 		newt->arr[i] = t->arr[i];
@@ -19,12 +23,18 @@ void htab_resize(htab_t *t, size_t newn) {
 
 	t->arr_size = newn;
 	t->arr = calloc(sizeof(htab_list_t *) * newn, 1);
+	if (t->arr == NULL) {
+		goto allocationError;
+	}
 
 	for (size_t i = 0; i < newt->arr_size; i++) {
 		htab_list_t *list = newt->arr[i];
 
 		while (list != NULL) {
 			htab_pair_t * addedList = htab_lookup_add(t, list->pair.key);
+			if (addedList == NULL) {
+				goto allocationError;
+			}
 			addedList->value = list->pair.value;
 
 			list = list->next;
@@ -32,4 +42,11 @@ void htab_resize(htab_t *t, size_t newn) {
 	}
 
 	htab_free(newt);
+	return;
+
+allocationError:
+	htab_free(newt);
+newtAllocationError:
+	htab_free(t);
+	exit(1);
 }
