3e89de9ae987861cbe3c96fe6a7f01fe7df5e70b
smatand <andrejsmatana0@gmail.com>
2022-04-18 22:29:52 +0200

    Add alternative (own) hash function
    Run with -DHASH_TEST

--- htab_hash_function.c
+++ htab_hash_function.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #include "htab_private.h"
+
+#ifndef HASH_TEST
 
 size_t htab_hash_function(const char *str) {
     uint32_t h=0;     // integer has to be of 32 bits
@@ -13,3 +16,26 @@ size_t htab_hash_function(const char *str) {
 
     return h;
 }
+
+#endif
+
+#ifdef HASH_TEST
+
+size_t htab_hash_function(const char *str) {
+    static int flag = 0;
+    if (flag == 0) {
+        flag = 1;
+        printf("Own hash_function used\n");
+    }
+
+    uint32_t h=853;     // integer has to be of 32 bits
+    const unsigned char *p;
+	
+    for(p=(const unsigned char*)str; *p!='\0'; p++) {
+        h = (h << 2) + 28841*h + *p;
+    }
+	
+    return h;
+} 
+
+#endif
